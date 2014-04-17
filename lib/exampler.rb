5d38d25a89b4f0ffa98b551f6d2599a2a54fd2b5
Mikhail Dronov <dronovmm@undev.ru>
2014-04-17 23:43:29 +0400

    added YAML support, methods and tests

--- lib/exampler.rb
+++ lib/exampler.rb
@@ -1,4 +1,11 @@
 require "exampler/version"
+require 'yaml'
+require 'tilt'
+require 'redcarpet'
+require 'fileutils'
+require 'date'
+require 'exampler/version'
+require 'exampler/post'
 
 module Exampler
   # Your code goes here...
